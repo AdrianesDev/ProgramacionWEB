107910a8d626ee9b5f65751647c8db405546ca41
ali <thealiavilez@gmail.com>
2025-03-27 10:00:58 -0700

    Ejercicio de Javascript

---
+++ Practicas/Ali-Avilez/JavaScript/01-Basicos.js
@@ -0,0 +1,92 @@
+//Variables y constantes
+const myString = "Este es una cadena de caracteres";
+const myNumber = 10;
+const myFloat = 10.451263;
+const myDouble = 10.2584848484;
+const myNull = null;
+const myUndefinde = undefined;
+
+//tipos de datos
+//script 
+//Number
+//Boll
+//Float
+//Doubles
+//Null
+//Undefinde
+
+//operadores
+// /
+// +
+// -
+// \*
+// %
+let suma = 5+5
+let resta = 5-5
+let multiplicacion = 5*5
+let division 5/5
+let rest = 5%5
+
+console.log(resta)
+console.log(Suma)
+console.log(division)
+console.log(multiplicacion)
+console.log(rest)
+
+//operadores de comparacion
+// > mayor que 
+// < menor que 
+// >= mayor igual
+// <= menor igual
+// == igual a
+// === estrictamente igual 
+// != diferente a
+// !== estrictamente diferente a 
+
+10 < 5
+10 > 5
+10 <= 5
+10 >= 5
+10 == 5
+10 === 5
+10 != 5
+10 !== 5
+
+// operadores basicos 
+// && AND
+// || OR
+// ! NOT
+
+true && true 
+true || true 
+!true 
+
+// true && true == true
+// true && false == false 
+// true || true == true 
+// true || false == true 
+// false || false == false 
+
+let Number1 = 1;
+let Number2 = 2;
+
+console.log(Sumatoria)
+let Sumatoria = Number1+Number2;
+let esMayor = Number1 > Number2;
+
+console.log (suma)
+console.log (esMayor)
+
+let palabra1 = 'Hola';
+let palabra2 = "Mundo";
+//cuando son 2 string y se hace una suma se le llama interpolacion
+// let frase = palabra1 + " " +  palabra2
+
+let frase  = `${Number1} ${Number2}`; 
+
+let name = "Ali";
+let surname = "Avilez";
+let age = "18";
+let frase = 'Hola mi nombre es ${name} tengo ${age} años, y mis apellidos son ${surname}';
+
+
