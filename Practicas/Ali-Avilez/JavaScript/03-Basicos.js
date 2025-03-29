48f32206b0dd8941c98ea43325d833641e549cd6
ali <thealiavilez@gmail.com>
2025-03-28 18:08:41 -0700

    Ejercicio de JavaScrpit 03

---
+++ Practicas/Ali-Avilez/JavaScript/03-Basicos.js
@@ -0,0 +1,10 @@
+let cali = 75
+if (cali<=60){
+  console.log('Reprobado');
+}else if (cali >=60 && cali <=69){
+  console.log('Bueno');
+}else if (cali >=70 && cali <=79){
+  console.log('Buena Calificacion')
+}else{
+  console.log('Excelente');
+}
