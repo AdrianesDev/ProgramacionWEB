430ce244c1e617da5ac326308f8f8fcf144585f1
Savitar-web <alanlosoya103gamer@gmail.com>
2025-04-04 06:39:43 -0700

    Tareas y actividades en clase

---
+++ Practicas/jorge-losoya/Más actividades/Switch/switch ejemplos.js
@@ -0,0 +1,33 @@
+let comida = "tacos"
+
+
+switch(comida) {
+    case "tacos":
+        console.log("comida mexicana")
+        break;
+        case "pizza":
+            console.log("comida americana")
+    case "tapas":
+        break;
+        console.log("comida europea")
+            break;
+            default:
+        console.log("comida de otra parte del mundo")
+}
+
+let semaforo = "verde"
+
+switch(semaforo) {
+    case "rojo":
+        console.log("Alto")
+        break;
+        case "Amarillo":
+            console.log("Baje la velocidad")
+    case "verde":
+        break;
+        console.log("Avance")
+            break;
+            default:
+        console.log("No existe ese color en el semaforo")
+}
+
